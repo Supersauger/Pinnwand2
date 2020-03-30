72c9fd470571dbea43b23fcb2e32090729890bb9
Supersauger <orhan_bjk98@hotmail.de>
2020-03-30 20:48:14 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static('./dist/myappname'));
+app.use(express.static('./dist/Pinnwand2'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join('./dist/myappname/index.html'));
+  res.sendFile(path.join('./dist/Pinnwand2/index.html'));
 });
 app.listen(process.env.PORT || 8080);
