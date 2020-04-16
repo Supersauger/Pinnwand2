ffad18d4c710e7809935d15f6a77654d97124729
Supersauger <orhan_bjk98@hotmail.de>
2020-04-16 15:28:21 +0200

    Update: Login/Register Fenster und Routing Part 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HauptmenuComponent} from './hauptmenu/hauptmenu.component';
 import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'hauptmenu', component: HauptmenuComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
