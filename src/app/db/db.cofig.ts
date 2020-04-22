86099e4bf59d5a77633ef90fbb41b157dc5699d2
niggi01 <56389368+niggi01@users.noreply.github.com>
2020-04-22 15:50:34 +0200

    mit db

---
+++ src/app/db/db.cofig.ts
@@ -0,0 +1,8 @@
+export const config = {
+  host: 'ec2-54-195-247-108.eu-west-1.compute.amazonaws.com',
+  user: 'fodxnyyjvemdgi',
+  password: '13b28f11105b263808b74e8b507856aae0dc0a5789b10f5c613b299fd1a4398b',
+  database: 'dbouih7qphiunh',
+  port: 5432,
+  ssl: {  rejectUnauthorized: false }
+};
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
-
-
+import {find2, findUser} from '../db/db.user';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
+
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+  }
+
 
   username: string;
   password: string;
@@ -18,6 +21,12 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  find() {
+    console.log('ENTERY find()');
+    findUser();
+  }
+
 
   login() {
     this.showSpinner = true;
