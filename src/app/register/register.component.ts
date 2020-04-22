0ee0ac8c00ba817a652af2966e3b475aa56b9e23
Supersauger <orhan_bjk98@hotmail.de>
2020-04-22 17:42:51 +0200

    Test

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,9 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
+  constructor() {
+  }
   username: string;
   password: string;
   showSpinner: boolean;
@@ -19,9 +18,12 @@ export class RegisterComponent implements OnInit {
 // todo
   register() {
     if (this.username === 'admin' && this.password === 'admin' && this.email === 'admin@admin.de') {
+
       alert('Danke für deine Registrierung. Bitte überprüf dein E-Mail Postfach nach einem Verfizierungslink.');
     } else {
       alert('Es existiert ein Account schon mit der E-Mail Adresse ' + this.email);
     }
   }
 }
+
+
