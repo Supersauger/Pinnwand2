a6c28a6f8efdf4de63c86da80e1c8165606ae780
Gidde <gideon.gratis@gmail.com>
2020-04-29 10:40:09 +0200

    Text wird nun am Ende des Pins abgeschnitten. Buttons in Big-Pin richtig alligned
    Es fehlt noch ein Indikator dass der text abgekürzt wurde.

--- src/app/mock-pins.ts
+++ src/app/mock-pins.ts
@@ -2,7 +2,7 @@ import { Pin } from './pin';
 
 export const PINS: Pin[] = [
     {title: 'pin 1', body: 'Body von Pin 1 lol ', date: 1585215340003, author: 'ehrendt'},
-    {title: 'pin 2', body: 'LoremLorLorem ILorem IpIpswumLorem IpswumLorem Ipswum ', date: 1500085245343, author: 'Ortürk'},
+    {title: 'pin 2', body: 'Hallo liebe Leser. Dies ist ein Beiepieltext um zu verdeutlichen wie die Overflow Eigenschaft sich auf den Zeilenumbruch und das Abschneiden eines Text am Ende des Elements auwirkt. Leider ist dieser Text nicht lang genug also muss ich jetzt irgendwelche Wörter dazu erfinden sodass dieser Text länger ist elas der Body von einem Pin ', date: 1500085245343, author: 'Ortürk'},
     {title: 'pin 3', body: 'Body von Pin1 lol ', date: 1585200015323, author: 'ehrendt'},
     {title: 'pin 4', body: 'Body von Pin1 lol ', date: 1585215300043, author: 'ehrendt'},
     {title: 'pin 5', body: 'Body von Pin1 lol ', date: 1500085245343, author: 'ehrendt'},
