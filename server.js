a6e2da78a778a36517bec711c47098badbed062d
jaarfi <day@arendt.cc>
2020-04-30 16:19:10 +0200

    Vllt angepasst für Heroku deployment

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ mongodb.MongoClient.connect(uri, function (err, client) {
   console.log("Database connection ready");
 
   // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
+  var server = app.listen(process.env.PORT, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
   });
--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -12,41 +12,36 @@ import {reject} from 'q';
 export class LoginService {
   constructor(private http: HttpClient) {
   }
-  dummerstring = 'data';
-  localUsers: User[] = [{bid: 1, name: 'nasd', passwort: 'askl fn', email: 'no'}];
-  getUsers(): User[] {
-    return this.localUsers;
-  }
 
   async getAllUsers() {
-    const result =  this.http.get('http://localhost:8080/api/users');
+    const result =  this.http.get('/api/users');
     return new Promise((resolve, reject) => {
       result.subscribe(resolve, reject);
     });
   }
   getAllUsersBackup(): Observable<User> {
-    return this.http.get<User>('http://localhost:8080/api/users');
+    return this.http.get<User>('api/users');
   }
   getUserByName(name: string): Observable<User> {
-    return this.http.get<User>('http://localhost:8080/api/users' + name);
+    return this.http.get<User>('/api/users' + name);
   }
   getUserByBID(bid: number): Observable<User> {
-    return this.http.get<User>('http://localhost:8080/api/users' + bid);
+    return this.http.get<User>('/api/users' + bid);
   }
   insertUser2(user: User): Observable<User> {
-    return this.http.post<User>('http://localhost:8080/api/users', user);
+    return this.http.post<User>('/api/users', user);
   }
   insertUser(user: User) {
-    const result =  this.http.post('http://localhost:8080/api/users', user);
+    const result =  this.http.post('/api/users', user);
     return new Promise((resolve, reject) => {
       result.subscribe(resolve, reject);
     });
   }
   deleteUserByName(name: string): Observable<User> {
-    return this.http.delete<User>('http://localhost:8080/api/users' + name);
+    return this.http.delete<User>('/api/users' + name);
   }
   updateUser(): Observable<User> {
     console.log('Currently not supported');
-    return this.http.get<User>('http://localhost:8080/api/users');
+    return this.http.get<User>('/api/users');
   }
 }
