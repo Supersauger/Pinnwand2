86bb5c9dca86f13c76e5e74a830effb90f27b298
jaarfi <day@arendt.cc>
2020-05-07 11:58:02 +0200

    GetUserByAttribute hinzugefügt

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -55,7 +55,7 @@ export class LoginService {
       .catch(this.handleError);
   }
   getUserByAttribute(elem: string, value: string): Promise<void | User[]> {
-    return this.http.get('http://localhost:8080/api/users/' + elem + value ) // Für Heroku die Localhost entfernen, Heroku: '/api/users' + name, // local: 'http://localhost:8080/api/users' + name
+    return this.http.get('/api/users/' + elem + value ) // Für Heroku die Localhost entfernen, Heroku: '/api/users' + name, // local: 'http://localhost:8080/api/users' + name
       .toPromise()
       .then(response => response as User[])
       .catch(this.handleError);
