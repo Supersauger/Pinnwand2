0c6d03f34836c36e9b879adaf1645aa8f83f937e
niggi01 <56389368+niggi01@users.noreply.github.com>
2020-06-03 18:11:51 +0200

    wieder rein

---
+++ src/app/big-pin/big-pin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-big-pin',
+  templateUrl: './big-pin.component.html',
+  styleUrls: ['./big-pin.component.css']
+})
+export class BigPinComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
